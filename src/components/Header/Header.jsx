c43f9f7d2bc5b4734c94a2955d3d77e25f64d423
sowmik-sec <sowmik.sec@gmail.com>
2024-07-05 11:31:23 +0600

    set some route in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+
 function Header() {
-  return <div>This is header</div>;
+  return (
+    <div>
+      <Link to={"/"}>Home</Link>
+      <Link to={"/login"}>Login</Link>
+    </div>
+  );
 }
 
 export default Header;
