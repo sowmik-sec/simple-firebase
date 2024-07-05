e65516e7739366701993af9fdbfdf2c3afa20fa4
sowmik-sec <sowmik.sec@gmail.com>
2024-07-05 11:42:52 +0600

    show user information after sign in with google

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,18 +1,28 @@
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import app from "../../firebase/firebase.init";
+import { useState } from "react";
 function Login() {
+  const [user, setUser] = useState(null);
   const auth = getAuth(app);
   const provider = new GoogleAuthProvider();
   const handleGoogleSingIn = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
         console.log(result.user);
+        setUser(result.user);
       })
       .catch((err) => console.log("error ", err.message));
   };
   return (
     <div>
       <button onClick={handleGoogleSingIn}>Google login</button>
+      {user && (
+        <div>
+          <h3>User: {user?.displayName}</h3>
+          <p>Email: {user.email}</p>
+          <img src={user.photoURL} alt="" />
+        </div>
+      )}
     </div>
   );
 }
